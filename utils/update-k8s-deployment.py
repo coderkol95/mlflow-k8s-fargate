3cbdd4c2b1aafadc098285d121164ae4799cf054
AnupamMisra <innocentangelfromhell@gmail.com>
2024-01-29 18:05:14 +0530

    Corrected the regex

--- utils/update-k8s-deployment.py
+++ utils/update-k8s-deployment.py
@@ -8,7 +8,7 @@ new_image_version=os.environ["IMAGE_TAG"]
 with open("../k8s-deployment.yaml","r") as f:
     k8s_yaml=f.read()
 
-image_version=re.findall('\d+\.\d+\.\d+',k8s_yaml)[0]
+image_version=re.findall('v\d+\.\d+\.\d+',k8s_yaml)[0]
 
 k8s_yaml_updated=re.sub(image_version,new_image_version,k8s_yaml)
 
